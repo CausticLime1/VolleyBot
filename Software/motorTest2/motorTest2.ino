bcb6bdf844f4b3e664824eae0bd7f3303a59e696
Ryan Su <ryansu2109@gmail.com>
2025-12-17 13:08:21 -0500

    updated startup code

--- Software/motorTest2/motorTest2.ino
+++ Software/motorTest2/motorTest2.ino
@@ -1,243 +1,192 @@
-#include <Wire.h>
+#define BUZZER_PIN PB8
+#define LED_PIN PB12
+#define IMU_SDA PB7
+#define IMU_CLK PB6
+#define M1_F PA0
+#define M1_B PA1
+#define M2_F PA2
+#define M2_B PA3
+#define M3_F PA6
+#define M3_B PA7
 
-float rollRate, pitchRate, yawRate;
-float rollCal, pitchCal, yawCal;
-float accX, accY, accZ;
-float roll, pitch;
+const int loopTimer = 1000;
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
 
-const int motor11APin = PA0;
-const int motor12APin = PA1;
+void startupBuzz(){
+  static int step = 0;
+  static long lastChange = 0;
+  currentMillis = millis();
 
-//Motor 2 Pins
+  switch (step){
+    case 0:
+      tone(BUZZER_PIN, 392);
+      step = 1;
+      lastChange = currentMillis;
 
-const int motor21APin = PA3;
-const int motor22APin = PA2;
+    case 1:
+      if (currentMillis - lastChange >= 400){
+        noTone(BUZZER_PIN);
+        lastChange = currentMillis;
+        step = 2;
+      }
+    case 2:
+      if (currentMillis - lastChange >= 200){
+        tone(BUZZER_PIN, 587);
+        lastChange = currentMillis;
+        step = 3;
+      }
+    case 3:
+      if (currentMillis - lastChange >= 500) {
+        noTone(BUZZER_PIN);
+        step = 4; 
+      }
+      break;
+    case 4: 
+      break;
+  }
+}
 
-//Motor 3 Pins
+void motorStartupTest(){
+  static int motorStep = 0;
+  currentMillis = millis();
+  static int motorLastChange = 0;
 
-const int motor31APin = PA6;
-const int motor32APin = PA7;
-
-//Motor Control
-
-float motor1Write = 0;
-float motor2Write = 0;
-float motor3Write = 0;
-
-//PID Control
-
-float rollError;
-float pitchError;
-float rollIntegral = 0;
-float pitchIntegral = 0;
-float rollDerivative = 0;
-float pitchDerivative = 0;
-float prevRoll = 0;
-float prevPitch = 0;
-float PIDRoll = 0;
-float PIDPitch = 0;
-
-//PID Tuning
-
-float gainP = 1;
-float gainI = 1;
-float gainD = 1;
-
-//Variables for the kalman filter
-float kalmanAngleRoll = 0, kalmanUncertaintyAngleRoll = 2*2;
-float kalmanAnglePitch = 0, kalmanUncertaintyAnglePitch = 2*2;
-float kalman1DOutput[] = {0, 0};
-
-uint32_t loopTimer; 
-
-
-void gyroSignals(void){
-
-  //Configure Gyroscope Scale
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1A);
-  Wire.write(0x05);
-  Wire.endTransmission();
-
-  //Configure Low-Pass Filter
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B);
-  Wire.write(0x8);
-  Wire.endTransmission();
-
-  //Configure Acceleration Scale
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1C);
-  Wire.write(0x10);
-  Wire.endTransmission();
-
-  //Reading Accelerometer
-  Wire.beginTransmission(0x68);
-  Wire.write(0x3B);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68, 6);
-  int16_t accXLSB = Wire.read() << 8 | Wire.read();
-  int16_t accYLSB = Wire.read() << 8 | Wire.read();
-  int16_t accZLSB = Wire.read() << 8 | Wire.read();
-
-  accX = (accXLSB / 4096.0) - 0.05;
-  accY = (accYLSB / 4096.0);
-  accZ = (accZLSB / 4096.0) -0.05;
-
-  //Reading Gyroscope 
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68, 6);
-  int16_t gyroX = Wire.read() << 8 | Wire.read();
-  int16_t gyroY = Wire.read() << 8 | Wire.read();
-  int16_t gyroZ = Wire.read() << 8 | Wire.read();
-
-  rollRate = (float)gyroX / 65.5;
-  pitchRate = (float)gyroY / 65.5;
-  yawRate = (float)gyroZ / 65.5;
+  switch (motorStep){
+    case 0:
+      analogWrite(M1_F, 128);
+      analogWrite(M2_F, 128);
+      analogWrite(M3_F, 128);
+      motorStep = 1;
+    case 1:
+      if (currentMillis - motorLastChange > 5000){
+        analogWrite(M1_F, 0);
+        analogWrite(M2_F, 0);
+        analogWrite(M3_F, 0);
+        motorStep = 2;
+      }
+    case 2:
+      break;
+  }
   
-  roll = atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * (180.0/M_PI);
-  pitch = atan(-accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * (180.0/M_PI);
 }
 
-void calibrateGyro() {
-  int32_t rollSum = 0, pitchSum = 0, yawSum = 0;
-  for (int i = 0; i < 500; i++) {
-    gyroSignals();
-    rollSum += rollRate;
-    pitchSum += pitchRate;
-    yawSum += yawRate;
-    delay(5);
+#define NOTE_C4  262
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_G4  392
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_C5  523
+
+int melody[] = {
+  NOTE_C4, NOTE_F4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_D4, NOTE_D4,
+  NOTE_D4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+  NOTE_C4, NOTE_A4, NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_D4,
+  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_G4, NOTE_E4, NOTE_F4
+};
+
+int durations[] = {
+  4, 4, 8, 8, 8, 8, 4, 4,
+  4, 4, 8, 8, 8, 8, 4, 4,
+  4, 4, 8, 8, 8, 8, 4, 4,
+  8, 8, 4, 4, 4, 2
+};
+
+int totalNotes = sizeof(melody) / sizeof(int);
+
+void updateStartupSequence() {
+  static int state = 0;             
+  static int noteIndex = 0;          
+  static unsigned long lastChange = 0;
+  static bool isNotePlaying = false; // The flag to prevent repeating calls
+  unsigned long now = millis();
+
+  int noteDuration; 
+
+  switch (state) {
+    case 0: // Startup Tone 1
+      if (!isNotePlaying) {
+        tone(BUZZER_PIN, 392);
+        isNotePlaying = true;
+        lastChange = now;
+      }
+      if (now - lastChange >= 200) {
+        noTone(BUZZER_PIN);
+        isNotePlaying = false; // Reset for next state
+        lastChange = now;
+        state = 1;
+      }
+      break;
+
+    case 1: // Startup Tone 2
+      if (!isNotePlaying) {
+        tone(BUZZER_PIN, 587);
+        isNotePlaying = true;
+        lastChange = now;
+      }
+      if (now - lastChange >= 500) {
+        noTone(BUZZER_PIN);
+        isNotePlaying = false; // Reset for next state
+        lastChange = now;
+        state = 2; 
+      }
+      break;
+
+    case 2: // The 1-Second Wait
+      if (now - lastChange >= 1000) {
+        lastChange = now;
+        state = 3; 
+      }
+      break;
+
+    case 3: // Christmas Song
+      noteDuration = 1000 / durations[noteIndex]; 
+      
+      // Only start the note if it's not already playing
+      if (!isNotePlaying) {
+        tone(BUZZER_PIN, melody[noteIndex]);
+        isNotePlaying = true;
+        lastChange = now;
+      }
+
+      // Logic to stop the note after duration
+      if (now - lastChange >= noteDuration) {
+        noTone(BUZZER_PIN);
+        
+        // Wait for a small "silence" gap (30% of note duration)
+        if (now - lastChange >= (noteDuration * 1.3)) {
+          isNotePlaying = false; // Allow next note to start
+          noteIndex++;
+          lastChange = now;
+          if (noteIndex >= totalNotes) state = 4;
+        }
+      }
+      break;
+
+    case 4: // Finished
+      noTone(BUZZER_PIN);
+      break;
   }
-  rollCal = rollSum / 500.0f;
-  pitchCal = pitchSum / 500.0f;
-  yawCal = yawSum / 500.0f;
 }
 
+void setup(){
+  pinMode(M1_F, OUTPUT);
+  pinMode(M1_B, OUTPUT);
+  pinMode(M2_F, OUTPUT);
+  pinMode(M2_B, OUTPUT);
+  pinMode(M3_F, OUTPUT);
+  pinMode(M3_B, OUTPUT);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(PB12, OUTPUT);
-  Wire.setSDA(PB7);
-  Wire.setSCL(PB6);
-  Wire.begin();
-  delay(250);
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission();
-
-  calibrateGyro();
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
-void analogTrimmer(float* input){
-  if (abs(*input) > 255){
-    if (*input > 0){
-      *input = 255;
-    }
-    else{
-      *input = -255;
-    }
+void loop(){
+  currentMillis = millis();
+  updateStartupSequence();
+  if (currentMillis - previousMillis >= loopTimer){
+    previousMillis = currentMillis;
   }
-}
-
-void kalman_1d(float* KalmanState, float* KalmanUncertainty, float KalmanInput, float KalmanMeasurement){
-  *KalmanState = *KalmanState + 0.004 * KalmanInput;
-  *KalmanUncertainty = *KalmanUncertainty + 0.004 * 0.004 * 4.0 * 4.0;
-  float KalmanGain = *KalmanUncertainty * (1/ ((*KalmanUncertainty) + 3.0*3.0));
-  *KalmanState = *KalmanState + KalmanGain * (KalmanMeasurement - *KalmanState);
-  *KalmanUncertainty = (1.0 - KalmanGain) * (*KalmanUncertainty);
-  kalman1DOutput[0] = *KalmanState;
-  kalman1DOutput[1] = *KalmanUncertainty;
-}
-
-void loop() {
-  gyroSignals();
-  rollRate -= rollCal;
-  pitchRate -= pitchCal;
-  yawRate -= yawCal;
-
-  //Filter of Roll Degrees
-  kalman_1d(&kalmanAngleRoll, &kalmanUncertaintyAngleRoll, rollRate, roll);
-  kalmanAngleRoll = kalman1DOutput[0];
-  kalmanUncertaintyAngleRoll = kalman1DOutput[1];
-
-  //Filter of Pitch Degrees
-  kalman_1d(&kalmanAnglePitch, &kalmanUncertaintyAnglePitch, pitchRate, pitch);
-  kalmanAnglePitch = kalman1DOutput[0];
-  kalmanUncertaintyAnglePitch = kalman1DOutput[1];
-
-  rollError = rollCal - kalmanAngleRoll;
-  pitchError = pitchCal - kalmanAnglePitch;
-
-  rollIntegral += rollError * 0.004;
-  pitchIntegral += pitchError * 0.004;
-
-  rollDerivative = (kalmanAngleRoll - prevRoll)/0.004;
-  pitchDerivative = (kalmanAnglePitch - prevPitch)/0.004;
-
-  PIDRoll = gainP * rollError + gainI * rollIntegral + gainD * rollDerivative;
-  PIDPitch = gainP * pitchError + gainI * pitchIntegral + gainD * pitchDerivative;
-
-  motor1Write = (-0.5 * PIDPitch) + (0.866 * PIDRoll); 
-  motor2Write = PIDRoll;
-  motor3Write = (-0.5 * PIDPitch) - (0.866 * PIDRoll);
-
-  if (motor1Write < 0){
-    motor1Write -= 64;
-    analogTrimmer(&motor1Write);
-    analogWrite(motor11APin, abs(motor1Write));
-    digitalWrite(motor12APin, 0);
-  }
-  else if (motor1Write > 0){
-    motor1Write += 64;
-    analogTrimmer(&motor1Write);
-    digitalWrite(motor11APin, 0);
-    analogWrite(motor12APin, abs(motor1Write));
-  }
-
-  if (motor2Write < 0){
-    motor2Write -= 64;
-    analogTrimmer(&motor2Write);
-    analogWrite(motor21APin, abs(motor2Write));
-    digitalWrite(motor22APin, 0);
-  }
-  else if (motor2Write > 0){
-    motor2Write += 64;
-    analogTrimmer(&motor2Write);
-    digitalWrite(motor21APin, 0);
-    analogWrite(motor22APin, abs(motor2Write));
-  }
-
-  if (motor3Write < 0){
-    motor3Write -= 64;
-    analogTrimmer(&motor3Write);
-    analogWrite(motor31APin, abs(motor3Write));
-    digitalWrite(motor32APin, 0);
-  }
-  else if (motor3Write > 0){
-    motor3Write += 64;
-    analogTrimmer(&motor3Write);
-    digitalWrite(motor31APin, 0);
-    analogWrite(motor32APin, abs(motor3Write));
-  }
-
-  prevRoll = kalmanAngleRoll;
-  prevPitch = kalmanAnglePitch;
-
-  Serial.print("Roll:");
-  Serial.print(kalmanAngleRoll);
-  Serial.print("\tPitch:");
-  Serial.print(kalmanAnglePitch);
-  Serial.print("\tMotor 1:");
-  Serial.print(motor1Write);
-  Serial.print("\tMotor 2:");
-  Serial.print(motor2Write);
-  Serial.print("\tMotor 3:");
-  Serial.println(motor3Write);
-
-  while (micros() - loopTimer < 4000);
-  loopTimer = micros();
+  
 }
